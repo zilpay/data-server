a93e17c5ef2c07e4d0fe851abfae64d97f96682c
Rinat <lich666black@gmail.com>
2022-02-14 12:40:55 +0300

    added migrations

--- src/orm.ts
+++ src/orm.ts
@@ -1,13 +1,22 @@
-import { MikroORM } from "@mikro-orm/core";
+import path from 'path';
+import { MikroORM, Utils } from "@mikro-orm/core";
 
 export async function initORM() {
-  return await MikroORM.init({
-    entities: [__dirname + '/src/models'],
-    entitiesTs: [__dirname + '/src/models/**/*.ts'],
+  const orm = await MikroORM.init({
+    migrations: {
+      path: path.join(__dirname, '../migrations'),
+    },
+    entities: [path.join(__dirname, '/models')],
+    entitiesTs: [path.join(__dirname, '/models/**/*.ts')],
     type: 'sqlite',
     dbName: 'dev.sqlite',
     debug: false,
     allowGlobalContext: true
   });
+  const migrator = orm.getMigrator();
+  await migrator.createMigration();
+  await migrator.up();
+
+  return orm;
 }
 initORM();
