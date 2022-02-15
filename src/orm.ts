c0af561b1daa0ab797f239195794dfb765ac781c
Rinat <lich666black@gmail.com>
2022-02-15 12:56:03 +0300

    created generator

--- src/orm.ts
+++ src/orm.ts
@@ -1,20 +1,6 @@
 import path from 'path';
 import { MikroORM } from "@mikro-orm/core";
 
-console.log({
-  migrations: {
-    path: path.join(__dirname, '../migrations'),
-  },
-  entities: [path.join(__dirname, '/models')],
-  entitiesTs: [path.join(__dirname, '/models/**/*.ts')],
-  type: String(process.env.TYPE) as "mongo" | "mysql" | "mariadb" | "postgresql" | "sqlite",
-  dbName: String(process.env.DB_NAME),
-  password: process.env.PASSWORD,
-  user: process.env.USER,
-  debug: false,
-  allowGlobalContext: true
-});
-
 export async function initORM() {
   const orm = await MikroORM.init({
     migrations: {
@@ -29,9 +15,11 @@ export async function initORM() {
     debug: false,
     allowGlobalContext: true
   });
+  const generator = orm.getSchemaGenerator();
   const migrator = orm.getMigrator();
+  await generator.createSchema();
   await migrator.createMigration();
-  await migrator.up();
+  await migrator.up();  
 
   return orm;
 }
