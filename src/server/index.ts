fb48c1bf499de3d797393447ea6f19babb32e8b2
Rinat <lich666black@gmail.com>
2022-02-15 12:16:08 +0300

    added nati ddos

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,12 +4,16 @@ import express from 'express';
 import { router } from './routers';
 import { initORM } from '../orm';
  
+const DDDoS = require('dddos');
 // Initialize the express engine
 const app: express.Application = express();
  
 // Take a port 3000 for running server.
 const port: number = 3000;
 
+app.use(new DDDoS({
+  /*Configuration options*/
+}).express('ip', 'path'));
 app.use(express.json());
 app.use(router);
  
