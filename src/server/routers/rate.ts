065e049f0f9f1b1927dfa1050a720d9cbee81941
Rinat <lich666black@gmail.com>
2022-06-01 16:12:06 +0300

    added logs

--- src/server/routers/rate.ts
+++ src/server/routers/rate.ts
@@ -11,6 +11,7 @@ ratesRoute.get('/rates', async (req: Request, res: Response) => {
   try {
     const currency = req.query.currency ? String(req.query.currency).split(',') : undefined;
     const lastId = await rateRepo.count();
+    console.log(lastId);
     const ratesData = await rateRepo.findOne({
       id: lastId
     }, {
