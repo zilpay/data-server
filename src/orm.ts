172266c0044b16647a10830e0bb43ec8a4539510
Rinat <lich666black@gmail.com>
2022-06-01 14:55:31 +0300

    removed logs

--- src/orm.ts
+++ src/orm.ts
@@ -1,6 +1,24 @@
 import path from 'path';
 import { MikroORM } from "@mikro-orm/core";
 
+console.log({
+  migrations: {
+    path: path.join(__dirname, '../migrations'),
+  },
+  cache: {
+    enabled: false
+  },
+  entities: [path.join(__dirname, '/models')],
+  entitiesTs: [path.join(__dirname, '/models/**/*.ts')],
+  type: String(process.env.TYPE) as "mongo" | "mysql" | "mariadb" | "postgresql" | "sqlite",
+  dbName: String(process.env.DB_NAME),
+  password: process.env.PASSWORD,
+  user: process.env.USER,
+  debug: false,
+  allowGlobalContext: true,
+  port: Number(process.env.PORT)
+});
+
 export async function initORM() {
   const orm = await MikroORM.init({
     migrations: {
